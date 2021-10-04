3cd3cec8fa2d97164f4cadb04771a5ebc6ced459
yunqian <813143069@qq.com>
2021-10-04 23:38:52 +0800

    轮播图的基本显示

---
+++ src/components/base/slider/use-slider.js
@@ -0,0 +1,46 @@
+import BScroll from '@better-scroll/core'
+// slide 为 BetterScroll 扩展了轮播焦点图的能力
+import Slide from '@better-scroll/slide'
+/**
+ * 导入onXXX函数来注册生命周期钩子
+ * 只能在setup()期间同步使用
+ * 它们依赖于内部的全局状态来定位当前活动的实例
+ * 在没有当前活动实例的情况下调用它们将会出错
+ */
+import { onMounted, onUnmounted, ref } from 'vue'
+
+// 通过静态方法使用
+BScroll.use(Slide)
+
+// wrapperRef 接受参数，获取当前轮播图的ref
+export default function useSlider(wrapperRef) {
+  const slider = ref(null)
+  const currentPageIndex = ref(0)
+  // onMounted 对标 mounted
+  onMounted(() => {
+    // BScroll的options中传入slide相关的配置
+    const sliderVal = slider.value = new BScroll(wrapperRef.value, {
+      click: true,
+      scrollX: true, // 为true时设置slide的方向为横向
+      scrollY: false, // 为true时设置slide的方向为纵向，scrollX和scrollY不能同时设置为true
+      momentum: false, // 避免惯性动画带来的快速滚动时的闪烁问题和快速滑动时一次滚动多页的问题
+      bounce: false, // 设置为false，否则会在循环衔接时出现闪烁
+      probeType: 2, // 监听slideWillChange事件，在用户拖动slide时实时获取到slide的PageIndex的改变
+      slide: true // 开启slide功能，没有该项则插件不会生效
+    })
+    // 触发时机：轮播图的当前页值将要改变时
+    sliderVal.on('slideWillChange', (page) => {
+      // pageX：即将展示的横向页面的索引值，下标从0开始
+      currentPageIndex.value = page.pageX
+    })
+  })
+  // onUnmounted 对标 unmounted
+  onUnmounted(() => {
+    slider.value.destory()
+  })
+  // 返回当前页坐标 && 轮播图信息
+  return {
+    slider,
+    currentPageIndex
+  }
+}
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="recommend">
-    recommend
+    <!-- slide容器 -->
+    <div class="slider-wrapper">
+      <!-- slide滚动元素 -->
+      <div class="slider-content">
+        <!-- 至少需要一个轮播图 -->
+        <Slider v-if="sliders.length > 0" :sliders="sliders" />
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { getRecommend } from '@/service/recommend'
+import Slider from '@/components/base/slider/slider.vue'
 export default {
   name: 'recommend',
+  components: {
+    Slider
+  },
+  data() {
+    return {
+      sliders: []
+    }
+  },
   async created() {
+    // 获取轮播图数据
     const result = await getRecommend()
-    console.log(result)
+    this.sliders = result.sliders
   }
 }
 </script>
 <style lang="scss" scoped>
-
+.recommend {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  .slider-wrapper {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-top: 40%;
+    overflow: hidden;
+    .slider-content {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
