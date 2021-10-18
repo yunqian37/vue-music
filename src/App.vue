6693bc75aa3b35c5d47f2250b9a5f940c377ffd1
yunqian37 <813143069@qq.com>
2021-10-18 14:00:00 +0800

    播放器基础&歌曲播放

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
     <Header />
     <Tab />
     <router-view/>
+    <Player />
   </div>
 </template>
 <script>
 import Header from '@/components/header/header'
 import Tab from '@/components/tab/tab.vue'
+import Player from '@/components/player/player.vue'
 export default {
   components: {
     Header,
-    Tab
+    Tab,
+    Player
   }
 }
 </script>
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,6 @@
 // 计算属性 根据歌曲列表 和 当前索引得出当前播放歌曲是什么
 export const currentSong = (state) => {
-  return state.playlist[state.currentIndex]
+  console.log('state ==>', state.currentIndex)
+  console.log('state.playlist ==>', state.playList)
+  return state.playList[state.currentIndex] || {}
 }
