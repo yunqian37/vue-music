e0581eed6f5c28ba4bd6247f14482709ec35723c
yunqian <813143069@qq.com>
2021-10-24 10:48:00 +0800

    搜索页面初始化

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,13 +1,106 @@
 <template>
   <div class="search">
-    search
+    <div class="search-input-wrapper">
+      <SearchInput
+        v-model="query" />
+    </div>
+    <div class="search-content">
+      <div class="hot-keys">
+        <h1 class="title">热门搜索</h1>
+        <ul>
+          <li
+            class="item"
+            v-for="item in hotKeys"
+            :key="item.id"
+            @click="addQuery(item.key)">
+            <span>{{item.key}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import SearchInput from '@/components/search/search-input.vue'
+import { ref } from 'vue'
+import { getHotKeys } from '@/service/search'
 export default {
-  name: 'search'
+  name: 'search',
+  components: {
+    SearchInput
+  },
+  setup() {
+    const query = ref('')
+    const hotKeys = ref([])
+
+    getHotKeys().then((result) => {
+      hotKeys.value = result.hotKeys
+    })
+
+    function addQuery(s) {
+      query.value = s
+    }
+
+    return {
+      query,
+      hotKeys,
+      addQuery
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
-
+.search {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  .search-input-wrapper {
+    margin: 20px;
+  }
+  .search-content {
+    flex: 1;
+    overflow: hidden;
+    .hot-keys {
+      margin: 0 20px 20px 20px;
+      .title {
+        margin-bottom: 20px;
+        font-size: $font-size-medium;
+        color: $color-text-l;
+      }
+      .item {
+        display: inline-block;
+        padding: 5px 10px;
+        margin: 0 20px 10px 0;
+        border-radius: 6px;
+        background: $color-highlight-background;
+        font-size: $font-size-medium;
+        color: $color-text-d;
+      }
+    }
+    .search-history {
+      position: relative;
+      margin: 0 20px;
+      .title {
+        display: flex;
+        align-items: center;
+        height: 40px;
+        font-size: $font-size-medium;
+        color: $color-text-l;
+        .text {
+          flex: 1;
+        }
+        .clear {
+          @include extend-click();
+          .icon-clear {
+            font-size: $font-size-medium;
+            color: $color-text-d;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
